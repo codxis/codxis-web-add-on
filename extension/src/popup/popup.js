42f149000507ec85d8b19668c6e9d6fbcfe0d93f
José Rocha <joserochadev@gmail.com>
2026-03-25 09:36:06 -0300

    feat: atualiza versão da extensão para 1.0.1 e modifica mensagem de alerta

--- extension/src/popup/popup.js
+++ extension/src/popup/popup.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("btn");
-  
+
   if (!btn) return;
 
   btn.addEventListener("click", () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
-        func: () => alert("Olá da extensão!"),
+        func: () => alert("Olá da extensão! From Vercel"),
       });
     });
   });
